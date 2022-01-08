99540a908fc11bf107e41f86f7a2cf14821b57c7
holmir97 <holmir97@gmail.com>
2022-01-08 19:25:51 +0900

    fix: test controller

--- src/controller/test.ts
+++ src/controller/test.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
 
+// slack
+import slack from "../others/slack/slack";
+
 // libraries
 import response from "../library/response";
 import returnCode from "../library/returnCode";
@@ -18,6 +21,7 @@ const getTestController = async (req: Request, res: Response) => {
   try {
     const resData = await testService.getTestService();
 
+    // throw new Error();
     // 테스트 실패
     if (resData === -1) {
       response.basicResponse(res, returnCode.NOT_FOUND, "테스트 실패");
@@ -26,6 +30,7 @@ const getTestController = async (req: Request, res: Response) => {
       response.dataResponse(res, returnCode.OK, "테스트 성공", resData);
     }
   } catch (err) {
+    slack.slackWebhook(req, err.message);
     console.error(err.message);
     response.basicResponse(res, returnCode.INTERNAL_SERVER_ERROR, "서버 오류");
   }

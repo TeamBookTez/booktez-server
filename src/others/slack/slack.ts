7d80a998375eed43acccb44082fdff3d5a4265c4
holmir97 <holmir97@gmail.com>
2022-01-08 19:26:05 +0900

    feat: slack webhook

---
+++ src/others/slack/slack.ts
@@ -0,0 +1,19 @@
+import { error } from "console";
+import slackAPI from "./slackAPI";
+
+const slackWebhook = (req, message) => {
+  const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${
+    req.originalUrl
+  } ${message} 
+    ${JSON.stringify(error)}`;
+  slackAPI.sendMessageToSlack(
+    slackMessage,
+    slackAPI.DEV_WEB_HOOK_ERROR_MONITORING
+  );
+};
+
+const slack = {
+  slackWebhook,
+};
+
+export default slack;

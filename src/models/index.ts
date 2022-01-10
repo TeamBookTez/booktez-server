11066ec6e71959fef7201cb11e62b7cbe8278567
holmir97 <holmir97@gmail.com>
2022-01-10 22:14:36 +0900

    feat: table indexing

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,6 +2,7 @@ import { Sequelize } from "sequelize-typescript";
 import dotenv from "dotenv";
 import User from "./User";
 import Book from "./Book";
+import Review from "./Review";
 const db = {};
 
 dotenv.config();
@@ -21,7 +22,7 @@ export const sequelize = new Sequelize(
   }
 );
 
-sequelize.addModels([User, Book]);
+sequelize.addModels([User, Book, Review]);
 
-export { User, Book };
+export { User, Book, Review };
 export default sequelize;

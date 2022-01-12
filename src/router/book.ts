5297c6f55ec943edb89df4de10e311188d70f3aa
donggeun <geeneve@naver.com>
2022-01-12 22:53:55 +0900

    feat: add POST /book route

--- src/router/book.ts
+++ src/router/book.ts
@@ -2,11 +2,13 @@ import express from "express";
 const router = express.Router();
 
 // Middleware
+import isLoginMiddleware from "../middleware/isLogin";
 import authMiddleware from "../middleware/auth";
 
 // Controller
 import bookController from "../controller/book";
 
 router.get("/", authMiddleware, bookController.getBookController);
+router.post("/", isLoginMiddleware, bookController.postBookController);
 
 module.exports = router;

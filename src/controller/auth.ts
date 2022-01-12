0c07a75059a99c6515125c08d2f7cfe449091c7e
seohyun-106 <changseohyun@sookmyung.ac.kr>
2022-01-12 14:43:32 +0900

    fix: apply prettier

--- src/controller/auth.ts
+++ src/controller/auth.ts
@@ -144,7 +144,7 @@ const postLoginController = async (req: Request, res: Response) => {
   }
 };
 const authController = {
-    postSignupController,
+  postSignupController,
   postLoginController,
 };
 

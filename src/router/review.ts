a950bfd5f19a5297eb851e8208f60d485ad20bbf
holmir97 <holmir97@gmail.com>
2022-01-14 00:32:19 +0900

    feat: review route

---
+++ src/router/review.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import reviewController from "../controller/review";
+
+// middleware
+import authMiddleware from "../middleware/auth";
+
+const router = express.Router();
+
+// 독서 전
+router.post(
+  "/before/:isbn",
+  authMiddleware,
+  reviewController.postReviewController
+);
+
+module.exports = router;

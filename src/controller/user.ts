34fb8a999d6fb90b56fd8d46b99f239be5a66809
seohyun-106 <changseohyun@sookmyung.ac.kr>
2022-01-14 19:59:42 +0900

    feat: patch img api

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -41,8 +41,57 @@ const getMyInfoController = async (req: Request, res: Response) => {
   }
 };
 
+/**
+ *  @프로필이미지 수정
+ *  @route PATCH /user/img
+ *  @access public
+ *  @err 잘못된 폼데이터
+ */
+
+const patchImgController = async (req: Request, res: Response) => {
+  try {
+    const img = req.file.location ? req.file.location : null;
+    const resData = await userService.patchImgService(req.body.userID.id, img);
+
+    // 폼데이터 잘못된 경우
+    if (resData === constant.NULL_VALUE) {
+      response.basicResponse(
+        res,
+        returnCode.BAD_REQUEST,
+        false,
+        "필요한 데이터가 없습니다."
+      );
+    } else if (resData === constant.WRONG_IMG_FORM) {
+      response.basicResponse(
+        res,
+        returnCode.BAD_REQUEST,
+        false,
+        "잘못된 폼 데이터입니다."
+      );
+    }
+
+    // 모두 성공시
+    response.basicResponse(
+      res,
+      returnCode.OK,
+      true,
+      "프로필 이미지 변경 완료."
+    );
+  } catch (err) {
+    slack.slackWebhook(req, err.message);
+    console.error(err.message);
+    response.basicResponse(
+      res,
+      returnCode.INTERNAL_SERVER_ERROR,
+      false,
+      "서버 오류"
+    );
+  }
+};
+
 const userController = {
   getMyInfoController,
+  patchImgController,
 };
 
 export default userController;

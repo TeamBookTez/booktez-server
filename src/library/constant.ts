2f2db014f78bf63ee572cf604a3e09cf02ec12da
seohyun-106 <changseohyun@sookmyung.ac.kr>
2022-01-14 13:03:53 +0900

    feat: get review api

--- src/library/constant.ts
+++ src/library/constant.ts
@@ -6,6 +6,7 @@ export default {
   WRONG_PASSWORD_CONVENTION: -4, // 비밀번호 형식이 잘못 되었을 때
   EMAIL_ALREADY_EXIST: -5, // 이미 존재하는 이메일 일 때
   NICKNAME_ALREADY_EXIST: -6, // 이미 존재하는 닉네임 일 때
-  WRONG_REQUEST_VALUE: -7, // 잘못된 요청값이 들어왔을 때
-  VALUE_ALREADY_EXIST: -8, // 이미 존재하는 값일 때
+  WRONG_IMG_FORM: -7, // 잘못된 이미지 폼일 때
+  WRONG_REQUEST_VALUE: -8, // 잘못된 요청값이 들어왔을 때
+  VALUE_ALREADY_EXIST: -9, // 이미 존재하는 값일 때
 };

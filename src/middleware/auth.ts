d48029515c114b810b21164a4abd87a6461d33cf
seohyun-106 <changseohyun@sookmyung.ac.kr>
2022-01-17 14:01:17 +0900

    fix: add req.user in auth middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -10,7 +10,10 @@ import returnCode from "../library/returnCode";
 // slack
 import slack from "../others/slack/slack";
 
-export default (req: Request, res: Response, next) => {
+import User from "../models/User";
+import { promises } from "fs";
+
+export default async(req: Request, res: Response, next) => {
   // 토큰 검사
   if (req.headers.authorization == null) {
     response.basicResponse(
@@ -19,13 +22,21 @@ export default (req: Request, res: Response, next) => {
       false,
       "토큰 값이 요청되지 않았습니다"
     );
-  } 
+  }
   const token = req.headers.authorization;
 
   // Verify token
   try {
     const decoded = jwt.verify(token, config.jwtSecret);
-    req.body.userID = decoded.user;
+    req.body.userID = decoded.user; // 이따 여기를
+
+    const user = await User.findOne({
+      where: { id: decoded.user.id, isDeleted: false },
+    });
+
+    // 이걸로 바꾸면 되는 ..
+    // req.user = user;
+
     next();
   } catch (err) {
     if (err.message === "jwt expired") {

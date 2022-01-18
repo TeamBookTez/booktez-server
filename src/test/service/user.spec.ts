132b3643e6eb50f80d56452056785582a3d02070
seohyun-106 <changseohyun@sookmyung.ac.kr>
2022-01-19 02:34:24 +0900

    test: add getMyInfo test code

--- src/test/service/user.spec.ts
+++ src/test/service/user.spec.ts
@@ -1,2 +1,46 @@
 import assert from "assert";
 import userService from "../../service/user";
+import User from "../../models/User";
+import { getAssociationsByRelation } from "sequelize-typescript";
+
+describe("userService test", () => {
+  let user;
+  const email = "mocha@test.com";
+  const nickname = "mocha";
+  const password = "mocha123!!";
+
+  before("create user", async() =>{
+    user = await User.create({
+      email,
+      nickname,
+      password
+    });
+  });
+
+  after("delete user", async() => {
+    await User.destroy({where: {id: user.id}});
+  });
+
+  describe("getMyInfo test", () => {
+    it("getMyInfo returns user Info correctly", async() => {
+      const createdUser: any = await User.findOne({where: {id: user.id}});
+      const testedUser: any = await userService.getMyInfoService(user.id);
+      assert.ok(createdUser.img === testedUser.img);
+      assert.ok(createdUser.nickname === testedUser.nickname);
+      assert.ok(createdUser.email === testedUser.email);
+      assert.ok(testedUser.reviewCount === 0);
+
+      const test = await userService.getMyInfoService(null);
+    });
+  });
+
+  describe("patchImg test", () => {
+    before("save original img", async() => {
+
+    });
+    after("put origin img", async() => {
+
+    });
+    it("", () => {});
+  });
+});

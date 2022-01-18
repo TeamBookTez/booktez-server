54029a8186d2a2e4fef9234f8423dd849d9acefc
seohyun-106 <changseohyun@sookmyung.ac.kr>
2022-01-19 03:22:29 +0900

    test: edit postBookService empty parameter to null

--- src/test/service/user.spec.ts
+++ src/test/service/user.spec.ts
@@ -1,5 +1,6 @@
 import assert from "assert";
 import userService from "../../service/user";
+import constant from "../../library/constant";
 import User from "../../models/User";
 import { getAssociationsByRelation } from "sequelize-typescript";
 
@@ -9,38 +10,41 @@ describe("userService test", () => {
   const nickname = "mocha";
   const password = "mocha123!!";
 
-  before("create user", async() =>{
+  before("create user", async () => {
     user = await User.create({
       email,
       nickname,
-      password
+      password,
     });
   });
 
-  after("delete user", async() => {
-    await User.destroy({where: {id: user.id}});
+  after("delete user", async () => {
+    await User.destroy({ where: { id: user.id } });
   });
 
   describe("getMyInfo test", () => {
-    it("getMyInfo returns user Info correctly", async() => {
-      const createdUser: any = await User.findOne({where: {id: user.id}});
+
+    it("success: getMyInfo returns user Info correctly", async () => {
+      const createdUser: any = await User.findOne({ where: { id: user.id } });
       const testedUser: any = await userService.getMyInfoService(user.id);
       assert.ok(createdUser.img === testedUser.img);
       assert.ok(createdUser.nickname === testedUser.nickname);
       assert.ok(createdUser.email === testedUser.email);
       assert.ok(testedUser.reviewCount === 0);
-
-      const test = await userService.getMyInfoService(null);
     });
+
+    it("fail: getMyInfo returns NULL_VALUE when userId is null", async () => {
+      assert.strictEqual(
+        await userService.getMyInfoService(null),
+        constant.NON_EXISTENT_USER
+      );
+    });
+    
   });
 
   describe("patchImg test", () => {
-    before("save original img", async() => {
-
-    });
-    after("put origin img", async() => {
-
-    });
+    before("save original img", async () => {});
+    after("put origin img", async () => {});
     it("", () => {});
   });
 });

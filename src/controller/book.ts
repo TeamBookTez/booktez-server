61d0fe9a6af2740c3189bf20201e0e1d887dc308
seohyun-106 <changseohyun@sookmyung.ac.kr>
2022-01-18 13:44:14 +0900

    fix: add userId parameter to postBookController

--- src/controller/book.ts
+++ src/controller/book.ts
@@ -22,6 +22,7 @@ const postBookController = async (req: Request, res: Response) => {
   try {
     const resData = await bookService.postBookService(
       req.user ? true : false,
+      req.user ? Number(req.user.id) : -1,
       req.body.isbn,
       req.body.thumbnail,
       req.body.title,
@@ -36,6 +37,28 @@ const postBookController = async (req: Request, res: Response) => {
         returnCode.BAD_REQUEST,
         false,
         "필요한 값이 없습니다."
+      );
+    }
+
+    if(resData == constant.VALUE_ALREADY_EXIST) {
+      return response.basicResponse(
+        res,
+        returnCode.BAD_REQUEST,
+        false,
+        "이미 독후감이 존재합니다."
+      );
+    }
+
+    if (resData == constant.ANONYMOUS_USER) {
+      return response.dataResponse(
+        res,
+        returnCode.OK,
+        "회원가입이 필요합니다.",
+        true,
+        {
+          isLogin: false,
+          reviewId: -1,
+        }
       );
     }
 

3542f3fcdcdc1bda7013153d74f0b5999e85805a
seohyun-106 <changseohyun@sookmyung.ac.kr>
2022-01-19 22:07:41 +0900

    feat: complete login flag check api

--- src/router/auth.ts
+++ src/router/auth.ts
@@ -1,6 +1,9 @@
 import express from "express";
 const router = express.Router();
 
+// Middleware
+import { isLogin } from "../middleware/authMiddleware";
+c
 // Controller
 import authController from "../controller/auth";
 
@@ -8,5 +11,6 @@ router.get("/email", authController.getEmailController);
 router.get("/nickname", authController.getNicknameController);
 router.post("/login", authController.postLoginController);
 router.post("/signup", authController.postSignupController);
+router.get("/check", isLogin, authController.getLoginFlagController);
 
 module.exports = router;

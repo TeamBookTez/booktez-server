de9f2a2180fe2f3f348804edebc39884125871bd
holmir97 <holmir97@gmail.com>
2022-02-20 16:42:47 +0900

    refactor: create review interface

---
+++ src/interface/IReview.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+export interface IBook {
+  _id: mongoose.Schema.Types.ObjectId;
+  isbn: Number;
+  isbn_sub: Number;
+  title: String;
+  author: [String];
+  translator: [String];
+  thumbnail: String;
+  publication_dt: String;
+}

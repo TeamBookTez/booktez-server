3e1331cfa899f7ac1ce7a9c3179986f4029cf424
holmir97 <holmir97@gmail.com>
2022-03-03 00:10:53 +0900

    refactor: add convention converter

--- src/library/convertSnakeToCamel.ts
+++ src/library/convertSnakeToCamel.ts
@@ -49,3 +49,14 @@ export const keysToSnake = (o) => {
 
   return o;
 };
+
+export const toSnakeString = (e) => {
+  return e
+    .match(/([A-Z])/g)
+    .reduce((str, c) => str.replace(new RegExp(c), "_" + c.toLowerCase()), e)
+    .substring(e.slice(0, 1).match(/([A-Z])/g) ? 1 : 0);
+};
+
+export const toCamelString = (e) => {
+  return e.replace(/_([a-z])/g, (g) => g[1].toUpperCase());
+};
